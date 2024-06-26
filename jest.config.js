ef21cf5f24dd9721cb10c572b8d491cc78e63c81
AlexandreJr <juniordias_@live.com>
2024-06-26 17:18:14 -0300

    chore: add coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -13,4 +13,12 @@ module.exports = {
     ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
 };
